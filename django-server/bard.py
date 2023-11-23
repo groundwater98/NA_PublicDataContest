0c4d8cf918558f341f2977f7ed803f2084a27056
groundwater98 <starprin3@gmail.com>
2023-11-23 18:50:17 +0900

    Fix: 기능 수정

--- django-server/bard.py
+++ django-server/bard.py
@@ -5,7 +5,6 @@ import config as cfg
 # BARD API KEY
 os.environ['_BARD_API_KEY'] = cfg.bard_config["api_key"]
 ''''''
-#form = cfg.bard_config["format"]
 human_content = cfg.bard_config["human"]
 environment_content = cfg.bard_config["environment"]
 labor_content = cfg.bard_config["labor"]
@@ -15,14 +14,13 @@ traffic_content = cfg.bard_config["traffic"]
 
 basic_form = cfg.bard_config["format"]
 
-
-
 def Human(user_situation):
     request = f"""The content included in the Human rights law is as follows:
                 content : [{human_content}].               
                 the specific situation: [{user_situation}]
                 When you receive the specific situation, please let us know the specific and realistic legal matters based on the laws of the National Assembly in Korea from the perspective of Korean human rights law experts. 
                 When you inform us of the legal matters, please fill out the basic form we provided. 
+                Please Please Do not provide answers at your discretion, but make sure to provide them according to the basic form you have received.
                 form: [{basic_form}]
                 The format provided includes relevant legal provisions, situation analysis, conclusions, answers, and additional considerations. Do not provide any answers other than this format.
                 """
@@ -37,6 +35,7 @@ def Environment(user_situation):
                 the specific situation: [{user_situation}]
                 When you receive the specific situation, please let us know the specific and realistic legal matters based on the laws of the National Assembly in Korea from the perspective of Korean environment law experts.
                 When you inform us of the legal matters, please fill out the basic form we provided.
+                Please Please Do not provide answers at your discretion, but make sure to provide them according to the basic form you have received.
                 form: [{basic_form}]
                 The format provided includes relevant legal provisions, situation analysis, conclusions, answers, and additional considerations. Do not provide any answers other than this format.
                 """
@@ -59,7 +58,7 @@ def Labor(user_situation):
     response = bardapi.core.Bard().get_answer(request)
 
     return response['content']
-#However, the content doesn't have to be the same, it just needs to be the same format.
+
 def Economy(user_situation):
     request = f"""The content included in the Economy law is as follows:
                 content : [{economy_content}].      
@@ -83,7 +82,6 @@ def Society(user_situation):
                 When you inform us of the legal matters, please fill out the basic form we provided.
                 Please Please Do not provide answers at your discretion, but make sure to provide them according to the basic form you have received.
                 form: [{basic_form}]
-                the specific situation: [{user_situation}]
                 The form provided includes relevant legal provisions, situation analysis, conclusions, answers, and additional considerations. Do not provide any answers other than this format.
                 """
     
