0d7e30bd36fc29d4d3694be935d53104e684dc92
jun9898 <nice1998@gmail.com>
2023-12-05 00:58:15 +0900

    Feat : 채팅창 화면 완성, 서버간 통신 작업중

--- springboot-server/server/src/main/java/com/example/server/controller/ChatController.java
+++ springboot-server/server/src/main/java/com/example/server/controller/ChatController.java
@@ -1,20 +1,26 @@
 package com.example.server.controller;
 
+import com.example.server.model.RequestChatDTO;
 import com.example.server.service.ChatService;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.RequestEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/chat")
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class ChatController {
 
     private final ChatService service;
 
     // 채팅이 이뤄지며 유저에게 반환할 데이터
     // Service단까지만 들어가고 DB에는 접근하지 않아도 됌
-//    public RequestEntity<String> chattingService(String message) {
-//    }
+    @PostMapping("/send")
+    public String chattingService(@RequestBody RequestChatDTO dto) {
+        System.out.println("dto = " + dto.toString());
+        return "통신 테스트";
+    }
 }
